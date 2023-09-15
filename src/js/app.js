9fbef764885ac729bcd273e8d50590b5b7643915
GoBDaNWeb <bogdan.ognitsiy@yandex.ru>
2023-09-15 12:54:54 +0300

    finish blog item page

---
+++ src/js/app.js
@@ -0,0 +1,161 @@
+import LazyLoad from 'vanilla-lazyload';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import { Fancybox } from "@fancyapps/ui";
+import { isBrowserSupportWebp } from './modules/support-webp.js';
+
+import { toggleModalWindow } from './modules/toggle-modal-window.js';
+import { animCircleText } from './modules/animate-circle.js';
+import { validInputs } from './modules/valid-inputs.js';
+import { formHandle } from './modules/form-handle.js';
+import { validateForm } from './modules/validate.js'
+import { handleMobileMenu } from './modules/mobile-menu.js';
+import { accordeonFunc } from './modules/accordeon.js';
+import { blogHandledFunc } from './modules/blog-handled.js'
+
+
+const swiper1 = new Swiper(".type-massage__swiper", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    modules: [Navigation, Pagination],
+    slidesPerView: 1.1,
+    spaceBetween: 16,
+    breakpoints: {
+        576: {
+            slidesPerView: 1.2,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 1.5,
+            spaceBetween: 20
+        },
+        992: {
+            slidesPerView: 2.5,
+            spaceBetween: 30
+        }
+    }
+});
+
+const swiper2 = new Swiper(".team__swiper", {
+    loop: true,
+    pagination: {
+        el: ".team__swiper-pagination",
+        type: "progressbar",
+    },
+    navigation: {
+        nextEl: ".team__swiper-button-next",
+        prevEl: ".team__swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+    slidesPerView: 1.1,
+    spaceBetween: 8,
+    breakpoints: {
+        576: {
+            slidesPerView: 1.5,
+            spaceBetween: 16
+        },
+        992: {
+            slidesPerView: 2.5,
+            spaceBetween: 16
+        },
+        1200: {
+            slidesPerView: 3.2,
+            spaceBetween: 16
+        },
+        1440: {
+            slidesPerView: 4.08,
+            spaceBetween: 16
+        }
+    }
+});
+
+
+const swiper3 = new Swiper(".blog__swiper", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+        nextEl: ".blog__swiper-button-next",
+        prevEl: ".blog__swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+    slidesPerView: 1.1,
+    spaceBetween: 16,
+    breakpoints: {
+        576: {
+            slidesPerView: 1.2,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 1.5,
+            spaceBetween: 20
+        },
+        992: {
+            slidesPerView: 2.5,
+            spaceBetween: 30
+        }
+    }
+});
+const swiper4 = new Swiper(".blog-item__swiper", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+        nextEl: ".blog-item__swiper-button-next",
+        prevEl: ".blog-item__swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+    slidesPerView: 1.1,
+    spaceBetween: 16,
+    breakpoints: {
+        576: {
+            slidesPerView: 1.2,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 1.5,
+            spaceBetween: 20
+        },
+        992: {
+            slidesPerView: 2.5,
+            spaceBetween: 30
+        }
+    }
+});
+
+Fancybox.bind('[data-fancybox="gallery"]', {
+    Thumbs : {
+        showOnStart: false
+    }
+});
+
+Fancybox.bind('[data-fancybox="gallery-files"]', {
+    Thumbs : {
+        showOnStart: false
+    }
+});
+
+
+
+isBrowserSupportWebp();
+//handleAttachFiles();
+
+toggleModalWindow();
+
+animCircleText();
+validInputs();
+formHandle();
+validateForm();
+handleMobileMenu();
+accordeonFunc();
+blogHandledFunc();
+
+let myLazyLoad = new LazyLoad();
+myLazyLoad.update();
---
+++ src/js/modules/toggle-modal-window.js
@@ -0,0 +1,74 @@
+export const toggleModalWindow = () => {
+    const btnOpenModal = document.querySelectorAll('.modal-window__btn-open');
+    const btnShortOpenModal = document.querySelectorAll('.modal-window__btn-short-open');
+    const htmlWindow = document.querySelector('html');
+    
+    const modal = document.querySelector('.modal-window');
+
+    const btnCloseModal = document.querySelectorAll('.modal-window__btn-close');
+
+    const btnSuccessOrderCloseModal = document.querySelectorAll('.form__btn-action-success');
+
+    let timeoutEvent;
+
+
+
+    const openModalWindow = (modal, classModal) => {
+        htmlWindow.classList.add('active');
+        modal.classList.add('active');
+        const modalWindowContent = modal.querySelector(classModal);
+        modalWindowContent.classList.add('active');
+    }
+
+    const closeModalWindow = (modal) => {
+        htmlWindow.classList.remove('active');
+        let orderWindow = modal.querySelector('.modal-window__content-order');
+        let orderShortWindow = modal.querySelector('.modal-window__content-short-order');
+        modal.classList.remove('active');
+        let successWindow = modal.querySelector('.modal-window__content-success');
+        timeoutEvent = setTimeout(() => {
+            successWindow.classList.remove('active');
+            orderWindow.classList.remove('active');
+            orderShortWindow.classList.remove('active');
+        },1000)
+    }
+
+    const toggleModal = (btn) => {
+        let classModal = btn.classList.contains('modal-window__btn-short-open')
+            ? '.modal-window__content-short-order'
+            : '.modal-window__content-order'
+        clearTimeout(timeoutEvent);
+        modal.classList.contains('active')
+            ? closeModalWindow(modal)
+            : openModalWindow(modal, classModal);
+    }
+
+    if (btnOpenModal) {
+        btnOpenModal.forEach(item => item.addEventListener('click', () => toggleModal(item)));
+    }
+
+    if (btnShortOpenModal) {
+        btnShortOpenModal.forEach(item => item.addEventListener('click', () => toggleModal(item)));
+    }
+
+    if (btnCloseModal) {
+        btnCloseModal.forEach(item => item.addEventListener('click', () => toggleModal(item)));
+    }
+
+    if (btnSuccessOrderCloseModal) {
+        btnSuccessOrderCloseModal.forEach(item => item.addEventListener('click', () => {
+            closeModalWindow(modal);
+        }));
+    }
+
+    const checkTarget = (event) => {
+        let elem = event.target;
+        if (elem.classList.contains('modal-window')) {
+            closeModalWindow(modal);
+        }
+    }
+
+    if (modal) {
+        modal.addEventListener('click', () => checkTarget(event))
+    }
+}
